2beef850339d557304110fd18f9b4f173913ae25
Jonas Molsted <jonasmolsted@gmail.com>
2025-12-28 14:15:16 +0100

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Gallery from './components/Gallery';
+import { Profile } from './components/Gallery';
+import './App.css'
+
+export default function App(){
+  return(
+    <Profile />
+  );
+}
